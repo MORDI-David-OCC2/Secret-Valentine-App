8dd9f236476805ac9a2aa401c06d49a7d5cce415
MORDI-David-OCC2 <david.mrd07@gmail.com>
2026-02-15 19:22:18 +0100

    Mauvaise correction

--- app_design_2/netlify/functions/importLinkToInbox.js
+++ app_design_2/netlify/functions/importLinkToInbox.js
@@ -141,7 +141,6 @@ exports.handler = async (event) => {
     const plainPreview = decryptPreviewMaybe(sourceInboxKey, data);
 
     const importedMessageId = `imp_${sourceInboxId}_${doc.id}`.slice(0, 150);
-    const importedMessageId2= importedMessageId.slice(29,)
 
     const clean = { ...data };
 

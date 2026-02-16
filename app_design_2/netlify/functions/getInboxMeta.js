1630d235f2c6866c21a30d2c507a23cff7687c58
MORDI-David-OCC2 <david.mrd07@gmail.com>
2026-02-16 21:55:37 +0100

    aad

--- app_design_2/netlify/functions/getInboxMeta.js
+++ app_design_2/netlify/functions/getInboxMeta.js
@@ -88,7 +88,7 @@ exports.handler = async (event) => {
     if (!inboxSnap.exists) return jsonResponse(404, { ok: false, error: "Inbox not found" });
 
     const d = inboxSnap.data() || {};
-    const pinRequired = !!(d.pinHash && d.pinSalt && d.pinIter);
+    const pinRequired = !!(d.passHash && d.passSalt && d.passIter);
 
     return jsonResponse(200, { ok: true, pinRequired });
   } catch (err) {
